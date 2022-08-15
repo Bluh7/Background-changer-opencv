e948d70e7d6f54fe0d7892656959bc583d3a1bcd
Gabriel Perazzo <94490806+Bluh7@users.noreply.github.com>
2022-08-15 16:12:38 -0300

    Update check_connected_webcams.py

--- check_connected_webcams.py
+++ check_connected_webcams.py
@@ -1,8 +1,12 @@
 import cv2
+import os
 
 def check_connected_webcams(webcam):
     while True:
-        cap_check = cv2.VideoCapture(webcam, cv2.CAP_DSHOW)
+        if os.name == "nt":
+            cap_check = cv2.VideoCapture(webcam, cv2.CAP_DSHOW)
+        else:
+            cat_check = cv2.VideoCapture(webcam)
         if cap_check.isOpened():
             webcam += 1
         elif cap_check is None or not cap_check.isOpened():
